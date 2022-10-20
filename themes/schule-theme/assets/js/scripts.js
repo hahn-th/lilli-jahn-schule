14819d34baba4382387c797c32f8f492bbeb68f5
username081581 <post@thomas-hahn.org>
2022-10-20 20:05:21 +0200

    shortcodes

--- themes/schule-theme/assets/js/scripts.js
+++ themes/schule-theme/assets/js/scripts.js
@@ -3,9 +3,10 @@ window.onscroll = function () { onScrollHandler() };
 
 // Get the header
 var header = document.getElementById("navbar");
+var sticky = header.getBoundingClientRect().y;
 
 function onScrollHandler() {
-  if (window.pageYOffset > 0) {
+  if (window.pageYOffset > sticky) {
     header.classList.add("sticky-shadow");
   }
   else {
@@ -13,8 +14,6 @@ function onScrollHandler() {
   }
 }
 
-//  // Get the offset position of the navbar
-//  var sticky = header.getBoundingClientRect().y;
 
 //  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 //  function myFunction() {
