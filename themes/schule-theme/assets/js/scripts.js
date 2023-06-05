f0578928a792a8cf962e94c6944ebef3cdbdde2e
username081581 <post@thomas-hahn.org>
2023-06-05 19:55:11 +0200

    Cookie Dialog

--- themes/schule-theme/assets/js/scripts.js
+++ themes/schule-theme/assets/js/scripts.js
@@ -1,5 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { onScrollHandler() };
+window.onscroll = function () {
+  onScrollHandler();
+};
 
 // Get the header
 var header = document.getElementById("navbar");
@@ -8,38 +10,45 @@ var sticky = header.getBoundingClientRect().y;
 function onScrollHandler() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky-shadow");
-  }
-  else {
+  } else {
     header.classList.remove("sticky-shadow");
   }
 }
 
-//  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-//  function myFunction() {
-//   var header = document.getElementById("main-navbar");
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString() + "; SameSite=Lax";
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
-//    if (window.pageYOffset > sticky) {
-//      header.classList.add("sticky-shadow");
-//    } else {
-//      header.classList.remove("sticky-shadow");
-//    }
-//  } 
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
 
+function setupCookieDialog() {
+  var accepted = getCookie("acceptedDatenschutz");
+  if (accepted != "true") {
+    var myModal = new bootstrap.Modal(
+      document.getElementById("acceptDatenschutz")
+    );
+    myModal.show();
+  }
+}
 
-// NAVBAR Stuff
+setupCookieDialog();
 
-// const navbarToggle = document.getElementById('navbar-toggle');
-// let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
-
-// const toggleNavbarVisibility = () => {
-//   isNavbarExpanded = !isNavbarExpanded;
-//   navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
-// };
-
-// navbarToggle.addEventListener('click', toggleNavbarVisibility);
-
-// const navbarMenu = document.getElementById('navbar-menu');
-// const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
-
-// navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
-// navbarMenu.addEventListener('click', toggleNavbarVisibility);
+const datenschutzModal = document.getElementById("acceptDatenschutz");
+datenschutzModal.addEventListener("hidden.bs.modal", (event) => {
+  setCookie("acceptedDatenschutz", true, 180);
+});
